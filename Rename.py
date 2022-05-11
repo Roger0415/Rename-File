5b4b217596e3b37dd1af658e4bfc6a2f8f678561
Roger0415 <87884156+Roger0415@users.noreply.github.com>
2022-05-11 15:19:01 +0800

    Update Rename.py

--- Rename.py
+++ Rename.py
@@ -1,8 +1,8 @@
 import os
 
-path = "/tensorflow1/models/research/object_detection/images/train/"
+path = "Yours dir path , remenber useing / , don't use \ , otherwise that will be a wrong"
 files = os.listdir(path)
 print(f"Before Renaming: {files}")
 for i in range(len(files)):
-   os.rename(path+files[i], f"{path}00{i+1}.PNG")
+   os.rename(path+files[i], f"{path}filename{i+1}.PNG")
 print(f"After Renaming: {os.listdir(path)}")
